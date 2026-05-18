bf395c8e14e9f0eef3de9a12ba702d9973d16bd6
htbenwarp <3135927386@qq.com>
2026-05-18 18:04:58 +0800

    Update sidebar.tsx

--- src/features/theme/themes/fuwari/components/sidebar.tsx
+++ src/features/theme/themes/fuwari/components/sidebar.tsx
@@ -2,16 +2,20 @@ import { Suspense } from "react";
 import { cn } from "@/lib/utils";
 import { Profile } from "./profile";
 import { Tags, TagsSkeleton } from "./tags";
+import { SiteInfo } from "./site-info";
 
 export function Sidebar({ className }: { className?: string }) {
   return (
     <aside className={cn("flex flex-col gap-4", className)}>
+      {/* 个人信息 */}
       <div
         className="fuwari-onload-animation"
         style={{ animationDelay: "100ms" }}
       >
         <Profile />
       </div>
+
+      {/* 标签 */}
       <div
         className="sticky top-4 fuwari-onload-animation"
         style={{ animationDelay: "150ms" }}
@@ -19,6 +23,14 @@ export function Sidebar({ className }: { className?: string }) {
         <Suspense fallback={<TagsSkeleton />}>
           <Tags />
         </Suspense>
+      </div>
+
+      {/* 站点信息 */}
+      <div
+        className="fuwari-onload-animation"
+        style={{ animationDelay: "200ms" }}
+      >
+        <SiteInfo />
       </div>
     </aside>
   );
